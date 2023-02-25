adc258ae87968fbf8ffd7e931eef77595a2bbfa9
x.bonnel@pm.me <x.bonnel@pm.me>
2023-02-25 23:10:23 +0100

    new README and env for api key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import config from "./config";
 
 function App() {
   // Set up state to hold the background color, gif URL, and joke
@@ -8,8 +7,10 @@ function App() {
   const [gifUrl, setGifUrl] = useState("");
   const [jokeQuote, setJokeQuote] = useState("");
 
-  // Set up the URLs for the Giphy API and joke API using the API key from config.js
-  const GIPHY_API = `https://api.giphy.com/v1/gifs/random?api_key=${config.apiKey}&tag=stupid&rating=g`;
+  const GiphyApiKey = process.env.REACT_APP_GIPHY_API_KEY;
+
+  // Set up the URLs for the Giphy API and joke API using the API key from .env
+  const GIPHY_API = `https://api.giphy.com/v1/gifs/random?api_key=${GiphyApiKey}&tag=stupid&rating=g`;
   const jokeAPI =
     "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
 
@@ -29,7 +30,6 @@ function App() {
       const response = await fetch(`${GIPHY_API}&${Date.now()}`);
       const data = await response.json();
       const gif = data.data.images.original.url;
-      console.log(gif);
       setGifUrl(gif);
     } catch (error) {
       console.error("Failed to get GIF:", error);
@@ -42,7 +42,6 @@ function App() {
       const responseJoke = await fetch(`${jokeAPI}&${Date.now()}`);
       const dataJoke = await responseJoke.json();
       const joke = dataJoke.joke;
-      console.log(joke);
       setJokeQuote(joke);
     } catch (error) {
       console.error("Failed to get joke:", error);
@@ -60,7 +59,7 @@ function App() {
     <div className="App" style={{ backgroundColor }}>
       <header className="App-header">
         <h1>The stupid page</h1>
-        <img src={gifUrl} alt="" />
+        <img src={gifUrl} alt="" style={{ width: "400px" }} />
         <p className="quote">{jokeQuote}</p>
 
         <button
